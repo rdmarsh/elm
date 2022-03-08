e910b449defa62d9055480a13d3579405852c631
David Marsh <rdmarsh@gmail.com>
2022-03-08 21:07:15 +1100

    fixing issues with multiple filters #18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='elm',
-    version='0.9.1',
+    version='0.9.2',
     description='Install elm',
     url='https://github.com/rdmarsh/elm',
     author='David Marsh',
