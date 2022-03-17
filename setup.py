81ca74809b8d35d2e963bb576e8feac5208bf8fe
David Marsh <rdmarsh@gmail.com>
2022-03-17 17:37:15 +1100

    fixing not equals filter

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='elm',
-    version='0.9.8.2',
+    version='0.9.8.3',
     description='Install elm',
     url='https://github.com/rdmarsh/elm',
     author='David Marsh',
