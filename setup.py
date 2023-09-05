b34d7435c313d8b7d87d17f02bde2fbfa200fad1
David Marsh <rdmarsh@gmail.com>
2023-09-05 18:31:44 +1000

    allow escape of commas in filter fields

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='elm',
-    version='1.0.1',
+    version='1.0.2',
     description='Install elm',
     url='https://github.com/rdmarsh/elm',
     author='David Marsh',
