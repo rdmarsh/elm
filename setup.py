b8acbda7d03e984f27e8dc6eb85fb7e72dbe2967
David Marsh <rdmarsh@gmail.com>
2023-09-08 14:01:33 +1000

    fix issue #2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='elm',
-    version='1.0.3',
+    version='1.0.4',
     description='Install elm',
     url='https://github.com/rdmarsh/elm',
     author='David Marsh',
