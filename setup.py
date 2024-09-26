941bbc9630ca16c922771cc9b703c24872c29e9a
David Marsh <rdmarsh@gmail.com>
2024-09-26 23:25:56 +1000

    Applying recomendations

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='elm',
-    version='1.2.3',
+    version='1.2.4',
     description='Install elm',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/rdmarsh/elm',
     author='David Marsh',
     author_email='rdmarsh@gmail.com',
-    license='GPL-3.0',
+    license='GPLv3',
     py_modules=['elm'],
     install_requires=[
-        'Click',
+        'Click>=7.0,<9.0',  # Pinning version range
     ],
     entry_points={
         'console_scripts': [
-            'elm = elm:cli',
+            'elm=elm:cli',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        ],
+    python_requires='>=3.6',  # Define minimum Python version
 )
